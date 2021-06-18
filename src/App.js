f47ecb3d672d6fc7c357a8fe25036abf88928f1b
Raloseq <rafcio227@op.pl>
2021-06-18 12:51:00 +0200

    added preloader to app(fake data loading)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
 import { Route, Switch } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import BuildGenerator from "./pages/BuildGenerator";
+import BraumGif from "./images/braum-error.gif";
 import "./App.css";
 
 function App() {
   const [champions, setChampions] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+  }, []);
+
   useEffect(() => {
     const URL =
       "https://ddragon.leagueoflegends.com/cdn/11.11.1/data/en_US/champion.json";
@@ -28,20 +38,27 @@ function App() {
   }, [setChampions]);
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/">
-          <MainPage champions={champions} />
-        </Route>
-        {champions.map((item) => (
-          <Route
-            path={`/:${item.id}`}
-            key={item.id}
-            render={() => <BuildGenerator />}
-          >
-            <BuildGenerator />
+      {loading ? (
+        <Fragment>
+          <h1>Loading data...</h1>
+          <img src={BraumGif} alt="loading-gif"></img>
+        </Fragment>
+      ) : (
+        <Switch>
+          <Route exact path="/">
+            <MainPage champions={champions} />
           </Route>
-        ))}
-      </Switch>
+          {champions.map((item) => (
+            <Route
+              path={`/:${item.id}`}
+              key={item.id}
+              render={() => <BuildGenerator />}
+            >
+              <BuildGenerator />
+            </Route>
+          ))}
+        </Switch>
+      )}
     </div>
   );
 }
